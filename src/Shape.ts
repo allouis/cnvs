95048f6e844bc074fdeb0507c6469b0f8b66584e
Fabien O'Carroll <fabien@allou.is>
2014-02-16 20:13:15 +0000

    Adding Shape base class

---
+++ src/Shape.ts
@@ -0,0 +1,41 @@
+/// <reference path="Vector2D.ts" />
+
+module cnvs {
+
+  export interface Object2D {
+    position: Vector2D
+    render (ctx: any): void 
+  }
+
+  export interface ShapeOptions {
+    x: number
+    y: number
+    fill?: string
+    stroke?: string
+    border?: number
+  }
+
+  export class Shape implements Object2D {
+
+    public position: Vector2D 
+    public fill: string
+    public stroke: string
+    public border: number
+
+    constructor (options: ShapeOptions) {
+      this.position = new Vector2D(options.x, options.y);
+      this.fill = options.fill || 'black';
+      this.stroke = options.stroke || 'black';
+      this.border = options.border || 1;
+      minivents(this);
+    }
+
+    public render (ctx: any) {
+      ctx.fillStyle = this.fill;
+      ctx.strokeStyle = this.stroke;
+      ctx.lineWidth = this.border;
+    }
+
+  }
+
+}
