ca497c95e405cebcaf4030dff66317ae597396b4
Gorgi Kosev <gorgi.kosev@gmail.com>
2014-02-17 13:11:40 +0100

    shape and vector via commonjs

--- src/Vector2D.ts
+++ src/Vector2D.ts
@@ -1,45 +1,43 @@
-module cnvs {
-  export interface Vector {
-      x: number
-      y: number
-  }
+export interface Vector {
+    x: number;
+    y: number
+}
 
-  export class Vector2D implements Vector {
+export class Vector2D implements Vector {
 
-      x: number
-      y: number
+    x: number;
+    y: number;
 
-      constructor (x: number, y: number) {
-          this.x = x || 0
-          this.y = y || 0
-      }
+    constructor (x: number, y: number) {
+        this.x = x || 0
+            this.y = y || 0
+    }
 
-      public clone () {
-          return new Vector2D(this.x, this.y)
-      }
+    public clone () {
+        return new Vector2D(this.x, this.y)
+    }
 
-      public add (vector: Vector) {
-          this.x += vector.x
-          this.y += vector.y
-          return this
-      }
+    public add (vector: Vector) {
+        this.x += vector.x
+            this.y += vector.y
+            return this
+    }
 
-      public scale (scalar: number) {
-          this.x *= scalar
-          this.y *= scalar
-          return this
-      }
+    public scale (scalar: number) {
+        this.x *= scalar
+            this.y *= scalar
+            return this
+    }
 
-      public dot (vector: Vector) {
-          return this.x * vector.x + this.y * vector.y
-      }
+    public dot (vector: Vector) {
+        return this.x * vector.x + this.y * vector.y
+    }
 
-      public length () {
-          return Math.sqrt(this.dot(this))
-      }
+    public length () {
+        return Math.sqrt(this.dot(this))
+    }
 
-      public angle (vector: Vector2D) {
-          return Math.acos(this.dot(vector)/this.length()*vector.length())
-      }
-  }
+    public angle (vector: Vector2D) {
+        return Math.acos(this.dot(vector)/this.length()*vector.length())
+    }
 }
